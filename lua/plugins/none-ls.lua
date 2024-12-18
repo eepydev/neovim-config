d9b6c3e709245a1b857401a780e28a0ecf6b0457
eepydev <mail>
2024-12-17 23:43:20 -0500

    first commit

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,12 @@
+return {
+  "nvimtools/none-ls.nvim",
+  opts = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+      },
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
+}
