574b77b74d6189bc8b9c3353c5160bcaa6305944
eepydev <mail>
2024-12-22 05:21:09 -0500

    changed lspconfig in order to use vim keybinding

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -20,7 +20,7 @@ return {
       lspconfig.lua_ls.setup({capabilities = capabilities })
       lspconfig.ts_ls.setup({capabilities = capabilities})
 
-      vim.keymap.set('n', 'k', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'o', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
